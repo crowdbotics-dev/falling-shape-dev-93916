08ca554082a5886305c6eba208c7c9cee5e9dde4
Crowdbotics <engineering@crowdbotics.com>
2023-06-22 23:23:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-shape-dev-93916",
+  url: "https://falling-shape-dev-93916.botics.co",
+  api: "https://falling-shape-dev-93916.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
